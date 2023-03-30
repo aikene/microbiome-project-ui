6b0d813ea223c58924bdd211b26708247f3d3b24
aikene <ikene.aj@gmail.com>
2023-03-29 22:25:33 -0400

    update path

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -2,7 +2,7 @@
 
 sudo systemctl daemon-reload
 systemctl restart gunicorn.service
-sudo cp /nginx/nginx.conf /etc/nginx/sites-available/django-aws_cicd
+sudo cp /home/ubuntu/django-aws_cicd/nginx/nginx.conf /etc/nginx/sites-available/django-aws_cicd
 sudo ln -s /etc/nginx/sites-available/django-aws_cicd /etc/nginx/sites-enabled
 sudo nginx -t
 sudo systemctl restart nginx
