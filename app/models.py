73de39ffb71f3cb6c779344859c74f9ce39a288f
Bing Yue <yueb81@gmail.com>
2023-04-25 02:34:47 -0500

    save selected runIds for visualization

--- app/models.py
+++ app/models.py
@@ -29,23 +29,23 @@ class History(models.Model):
         db_table = 'history'
 
 
+
 class Metadata(models.Model):
-    acc = models.TextField(unique=True, primary_key=True, verbose_name='Run ID')
-    # status = models.ForeignKey("Status", on_delete=models.CASCADE, related_name='Status')
+    acc = models.TextField(primary_key=True)
     assay_type = models.TextField(blank=True, null=True)
-    center_name = models.TextField(blank=True, null=True, verbose_name="Center Name")
+    center_name = models.TextField(blank=True, null=True)
     consent = models.TextField(blank=True, null=True)
     experiment = models.TextField(blank=True, null=True)
     sample_name = models.TextField(blank=True, null=True)
     instrument = models.TextField(blank=True, null=True)
-    librarylayout = models.TextField(blank=True, null=True, verbose_name='Library Layout')
+    librarylayout = models.TextField(blank=True, null=True)
     libraryselection = models.TextField(blank=True, null=True)
     librarysource = models.TextField(blank=True, null=True)
     platform = models.TextField(blank=True, null=True)
     sample_acc = models.TextField(blank=True, null=True)
     biosample = models.TextField(blank=True, null=True)
     organism = models.TextField(blank=True, null=True)
-    sra_study = models.TextField(blank=True, null=True, verbose_name="SRA Study ID")
+    sra_study = models.TextField(blank=True, null=True)
     releasedate = models.DateTimeField(blank=True, null=True)
     bioproject = models.TextField(blank=True, null=True)
     mbytes = models.IntegerField(blank=True, null=True)
@@ -64,17 +64,17 @@ class Metadata(models.Model):
     datastore_filetype = models.TextField(blank=True, null=True)
     attributes = models.TextField(blank=True, null=True)
     jattr = models.TextField(blank=True, null=True)
-    ethnicity_sam = models.TextField(blank=True, null=True)
-    race_sam = models.TextField(blank=True, null=True)
+    ecotype_sam = models.TextField(blank=True, null=True)
+    cultivar_sam = models.TextField(blank=True, null=True)
+    breed_sam = models.TextField(blank=True, null=True)
+    strain_sam = models.TextField(blank=True, null=True)
+    iosolate_sam = models.TextField(blank=True, null=True)
     race_ethnicity = models.TextField(blank=True, null=True)
-    host_sam = models.TextField(blank=True, null=True)
-    gender_extract = models.TextField(blank=True, null=True)
     gender = models.TextField(blank=True, null=True)
 
     class Meta:
         managed = False
         db_table = 'metadata'
-
 
 class Status(models.Model):
     acc = models.OneToOneField(Metadata, models.DO_NOTHING, db_column='acc', primary_key=True)
--- app/utils.py
+++ app/utils.py
@@ -8,3 +8,26 @@ def validate_password(password):
         return False, f'Your password cannot be entirely numeric.'
 
     return True, 'Your password has been updated successfully.'
+
+
+def update_session_list(request,key, runId, add=1):
+    # Get the session variable
+    session_list = request.session.get(key, [])
+
+    # Check if the input runId value is in the list
+    if runId in session_list:
+        # Remove the input runId value from the list
+        if add == 0:
+            session_list.remove(runId)
+    else:
+        # Add the input runId value to the list
+        if add == 1:
+            session_list.append(runId)
+
+    # Update the session variable
+    request.session[key] = session_list
+
+
+def clear_session_list(request,key):
+    request.session[key] = []
+        
