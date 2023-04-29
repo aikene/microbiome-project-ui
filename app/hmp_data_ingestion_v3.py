d75a9f48472e69f2e6b8cff3fe2129e28bd2611c
Nate Landi <emeraldmane@gmail.com>
2023-04-29 19:19:02 -0400

    adding example outputs

--- app/hmp_data_ingestion_v3.py
+++ app/hmp_data_ingestion_v3.py
@@ -24,10 +24,29 @@ def get_sra_hmp_uuid(sra_id): # example input is sra_id='"SRP115494"'
     
     # connecting sra id with hmp id with associated filenames
     hmp_df = hmp_df.rename(columns={"id": "hmp_id"})
-    hmp_df['sra_id'] = sra_id
+    hmp_df['sra_id'] = sra_id.replace('"', '')
     hmp_df['sra_hmp_uuid'] = hmp_df['sra_id']+'_'+hmp_df['hmp_id']
     hmp_df = hmp_df[['sra_hmp_uuid','sra_id','hmp_id','file_name']]
 
     return hmp_df
 
+sra_id='"SRP115494"'
 sra_hmp_ids = get_sra_hmp_uuid(sra_id)
+print(sra_hmp_ids.tail())
+
+#
+# EXAMPLE OUTPUT:
+#
+#                                   sra_hmp_uuid     sra_id                            hmp_id
+# 43  SRP115494_7cfd74d6803ea319683df7564432f9bf  SRP115494  7cfd74d6803ea319683df7564432f9bf  \
+# 44  SRP115494_56bda9b020293b4b1d65e1eb253ee792  SRP115494  56bda9b020293b4b1d65e1eb253ee792   
+# 45  SRP115494_7cfd74d6803ea319683df7564422f960  SRP115494  7cfd74d6803ea319683df7564422f960   
+# 46  SRP115494_d39c1941c8f6e8b0f6ead5d7f729e885  SRP115494  d39c1941c8f6e8b0f6ead5d7f729e885   
+# 47  SRP115494_d39c1941c8f6e8b0f6ead5d7f72a15d3  SRP115494  d39c1941c8f6e8b0f6ead5d7f72a15d3   
+
+#                                             file_name
+# 43  https://downloads.hmpdacc.org/ihmp/t2d/genome/...
+# 44  https://downloads.hmpdacc.org/ihmp/t2d/genome/...
+# 45  https://downloads.hmpdacc.org/ihmp/t2d/genome/...
+# 46  https://downloads.hmpdacc.org/ihmp/t2d/genome/...
+# 47  https://downloads.hmpdacc.org/ihmp/t2d/genome/...
