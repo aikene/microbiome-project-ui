c7e0d04803dcce73905337abd57edd1e2d3fcfce
aikene <ikene.aj@gmail.com>
2023-04-28 20:05:26 -0400

    - move collectstatic to deploy

--- scripts/python_dependencies.sh
+++ scripts/python_dependencies.sh
@@ -3,3 +3,5 @@
 virtualenv /home/ubuntu/env
 source /home/ubuntu/env/bin/activate
 pip install -r /home/ubuntu/django-aws_cicd/requirements.txt
+cd /home/ubuntu/django-aws_cicd/
+python3 manage.py collectstatic --noinput
