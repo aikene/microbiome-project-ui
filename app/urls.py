4491ac33f68386bc692d0541fb1dd8741f78308b
Nate Landi <emeraldmane@gmail.com>
2023-04-29 17:21:45 -0400

    Adding in import_data url

--- app/urls.py
+++ app/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
     path("profile/<str:username>", views.user_profile, name="profile"),
+    path("import_data", views.import_data, name="import_data"),   # <- added by nlandi
     path("visualization/", views.visualization, name="visualization"),
 ]
