cc93dd42b6185d6932a56113df42193bb6712e51
aikene <ikene.aj@gmail.com>
2023-04-28 20:21:45 -0400

    - copy environment file to directory

--- scripts/python_dependencies.sh
+++ scripts/python_dependencies.sh
@@ -3,5 +3,7 @@
 virtualenv /home/ubuntu/env
 source /home/ubuntu/env/bin/activate
 pip install -r /home/ubuntu/django-aws_cicd/requirements.txt
+
+cp /home/ubuntu/.env /home/ubuntu/django-aws_cicd/awscicd/.env
 cd /home/ubuntu/django-aws_cicd/
 python3 manage.py collectstatic --noinput
